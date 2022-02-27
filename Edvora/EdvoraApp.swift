c0b555923e5bed0e42aff58268074e874c6e8689
Thomas <akinniyithomas@yahoo.com>
2022-02-27 22:24:12 +0100

    Initial Commit

---
+++ Edvora/EdvoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdvoraApp.swift
+//  Edvora
+//
+//  Created by Decagon on 27/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct EdvoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
